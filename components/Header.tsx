a73ed22ae5a6f85f7634022bfc56eff3c3962cd5
James Aguirre <jamesaguirre98@gmail.com>
2023-09-15 15:10:37 -0700

    intial commit

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import { twMerge } from "tailwind-merge";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className: string;
+}
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  return (
+    <div
+      className={twMerge(
+        `h-fit
+    bg-gradient-to-b from-emerald-800
+    p-6`,
+        className
+      )}
+    >
+      Hello World!
+    </div>
+  );
+};
+
+export default Header;
